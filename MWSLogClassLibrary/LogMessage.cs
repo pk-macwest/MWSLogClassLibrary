ed80756d02485a560261cf1e7f7d34d4858306fa
Peter Kelly <peter.kelly@macwestsolutions.com>
2019-05-08 08:38:00 +0800

    added additional functionality for  'BIM 360 Exchange'

--- MWSLogClassLibrary/LogMessage.cs
+++ MWSLogClassLibrary/LogMessage.cs
@@ -17,240 +17,248 @@ public enum mwsEnumLogLevels
 }
 
 
-    public class mwsClassLogMessage
+public class mwsClassLogMessage
+{
+
+    public mwsClassLogMessage(mwsEnumLogLevels logLevel, String productName)
     {
- 
-        public mwsClassLogMessage(mwsEnumLogLevels logLevel, String productName)
+        pLogLevel = logLevel;
+        mwsProductName = productName;
+        pLogFile = Path.GetDirectoryName(Path.GetTempFileName()) + "\\MWSLogMessage.txt";
+    }
+    public mwsClassLogMessage(mwsEnumLogLevels logLevel, String productName, String fileName)
+    {
+        pLogLevel = logLevel;
+        mwsProductName = productName;
+        pLogFile = fileName;
+    }
+
+    /// <summary>
+    /// current log level
+    /// </summary>
+    public mwsEnumLogLevels pLogLevel { get; set; }
+    public String mwsProductName { get; set; }
+
+    public mwsEnumLogLevels mwsStringTologlevel(String value)
+    {
+        mwsEnumLogLevels logLevel = mwsEnumLogLevels.TYPE_INFORMATION;
+        switch (value)
         {
-            pLogLevel = logLevel;
-            mwsProductName = productName;
-            pLogFile = Path.GetDirectoryName(Path.GetTempFileName()) + "\\MWSLogMessage.txt";
-        }
-        public mwsClassLogMessage(mwsEnumLogLevels logLevel, String productName, String fileName)
-        {
-            pLogLevel = logLevel;
-            mwsProductName = productName;
-            pLogFile = fileName;
-        }
+            case "FATAL":
+                logLevel = mwsEnumLogLevels.TYPE_FATAL;
+                break;
 
-        /// <summary>
-        /// current log level
-        /// </summary>
-        public mwsEnumLogLevels pLogLevel { get; set; }
-        public String mwsProductName { get; set; }
+            case "ERROR":
+                logLevel = mwsEnumLogLevels.TYPE_ERROR;
+                break;
 
-        public mwsEnumLogLevels mwsStringTologlevel(String value)
-        {
-            mwsEnumLogLevels logLevel = mwsEnumLogLevels.TYPE_INFORMATION;
-            switch (value)
-            {
-                case "FATAL":
-                    logLevel = mwsEnumLogLevels.TYPE_FATAL;
-                    break;
+            case "WARNING":
+                logLevel = mwsEnumLogLevels.TYPE_WARNING;
+                break;
 
-                case "ERROR":
-                    logLevel = mwsEnumLogLevels.TYPE_ERROR;
-                    break;
+            case "INFORMATION":
+                logLevel = mwsEnumLogLevels.TYPE_INFORMATION;
+                break;
 
-                case "WARNING":
-                    logLevel = mwsEnumLogLevels.TYPE_WARNING;
-                    break;
+            case "DEBUG":
+                logLevel = mwsEnumLogLevels.TYPE_DEBUG;
+                break;
 
-                case "INFORMATION":
-                    logLevel = mwsEnumLogLevels.TYPE_INFORMATION;
-                    break;
-
-                case "DEBUG":
-                    logLevel = mwsEnumLogLevels.TYPE_DEBUG;
-                    break;
-
-                case "TEST":
-                    logLevel = mwsEnumLogLevels.TYPE_TEST;
-                    break;
-            }
-
-            return logLevel;
-        }
-        /// <summary>
-        /// Product Name
-        /// </summary>
-        private String iProductName
-        {
-            get { return this.mwsProductName; }
-
+            case "TEST":
+                logLevel = mwsEnumLogLevels.TYPE_TEST;
+                break;
         }
 
-        private String iGetUserName
-        {
-            //get { return sThisAddIn.Application.UserName; }
-            get { return "Unknown"; }
-        }
+        return logLevel;
+    }
+    /// <summary>
+    /// Product Name
+    /// </summary>
+    private String iProductName
+    {
+        get { return this.mwsProductName; }
 
-        private String iGetVersion
-        {
-            get
-            {
-                String emwVersion = String.Empty;
+    }
 
-                emwVersion = GetType().Assembly.GetName().Version.ToString();
-                return emwVersion;
-            }
-        }
+    private String iGetUserName
+    {
+        //get { return sThisAddIn.Application.UserName; }
+        get { return "Unknown"; }
+    }
 
-
-        /// <summary>
-        /// Logging file handle
-        /// </summary>
-        private StreamWriter _iLogStream = null;
-        private StreamWriter iLogStream
-        {
-            get { return this._iLogStream; }
-            set { this._iLogStream = value; }
-        }
-
-
-        /// <summary>
-        /// Default log file name
-        /// </summary>
-        private String pLogFile
-        {
-            set;
-            get;
-        }
-
-
-        /// <summary>
-        /// Log file full path and name
-        /// </summary>
-        private String _iLogFile;
-        public String mwsLogFile
-        {
-            get { return this._iLogFile; }
-            set { this._iLogFile = value; }
-        }
-
-
-        public String mwsVersion()
+    private String iGetVersion
+    {
+        get
         {
             String emwVersion = String.Empty;
 
             emwVersion = GetType().Assembly.GetName().Version.ToString();
             return emwVersion;
         }
+    }
 
-        public void mwsLogMessage(mwsEnumLogLevels level, String text)
+
+    /// <summary>
+    /// Logging file handle
+    /// </summary>
+    private StreamWriter _iLogStream = null;
+    private StreamWriter iLogStream
+    {
+        get { return this._iLogStream; }
+        set { this._iLogStream = value; }
+    }
+
+
+    /// <summary>
+    /// Default log file name
+    /// </summary>
+    private String pLogFile
+    {
+        set;
+        get;
+    }
+
+
+    /// <summary>
+    /// Log file full path and name
+    /// </summary>
+    private String _iLogFile;
+    public String mwsLogFile
+    {
+        get { return this._iLogFile; }
+        set { this._iLogFile = value; }
+    }
+
+
+    public String mwsVersion()
+    {
+        String emwVersion = String.Empty;
+
+        emwVersion = GetType().Assembly.GetName().Version.ToString();
+        return emwVersion;
+    }
+
+    public void mwsLogMessage(mwsEnumLogLevels level, String text)
+    {
+        mwsLogMessage(null, level, text);
+    }
+
+    public String mwsGetLogLevel(mwsEnumLogLevels loglevel)
+    {
+        String returnValue = String.Empty;
+
+        switch (this.pLogLevel)
         {
-            mwsLogMessage(null, level, text);
+            case mwsEnumLogLevels.TYPE_ERROR:
+                returnValue = "Error";
+                break;
+            case mwsEnumLogLevels.TYPE_FATAL:
+                returnValue = "Fatal";
+                break;
+            case mwsEnumLogLevels.TYPE_INFORMATION:
+                returnValue = "Information";
+                break;
+            case mwsEnumLogLevels.TYPE_NONE:
+                returnValue = "None";
+                break;
+            case mwsEnumLogLevels.TYPE_WARNING:
+                returnValue = "Warning";
+                break;
+            case mwsEnumLogLevels.TYPE_DEBUG:
+                returnValue = "Debug";
+                break;
         }
+        return returnValue;
+    }
 
-        public String mwsGetLogLevel(mwsEnumLogLevels loglevel)
+    public String mwsGetLogLevel()
+    {
+        return mwsGetLogLevel(this.pLogLevel);
+    }
+
+    public void mwsLogMessage(StackTrace est, mwsEnumLogLevels level, String text)
+    {
+        StackTrace st = new StackTrace(true);
+        String stringLevel = String.Empty;
+        String logDateTime = DateTime.Now.ToString();
+
+        // Check current log level
+        if (pLogLevel < level) return;
+
+        switch (level)
         {
-            String returnValue = String.Empty;
+            case mwsEnumLogLevels.TYPE_FATAL:
+                stringLevel = "Fatal";
+                wmsLogMessage(iProductName + " Version " + mwsVersion() + " " + logDateTime + " " + stringLevel + ": (" + st.GetFrame(1).GetMethod().Name + ") - " + text);
+                break;
 
-            switch (this.pLogLevel)
-            {
-                case mwsEnumLogLevels.TYPE_ERROR:
-                    returnValue = "Error";
-                    break;
-                case mwsEnumLogLevels.TYPE_FATAL:
-                    returnValue = "Fatal";
-                    break;
-                case mwsEnumLogLevels.TYPE_INFORMATION:
-                    returnValue = "Information";
-                    break;
-                case mwsEnumLogLevels.TYPE_NONE:
-                    returnValue = "None";
-                    break;
-                case mwsEnumLogLevels.TYPE_WARNING:
-                    returnValue = "Warning";
-                    break;
-                case mwsEnumLogLevels.TYPE_DEBUG:
-                    returnValue = "Debug";
-                    break;
-            }
-            return returnValue;
+            case mwsEnumLogLevels.TYPE_ERROR:
+                stringLevel = "Error";
+                wmsLogMessage(iProductName + " Version " + mwsVersion() + " " + logDateTime + " " + stringLevel + ": (" + st.GetFrame(1).GetMethod().Name + ") - " + text);
+                wmsLogMessage("Strack Trace from error:");
+                for (int i = 1; i < st.FrameCount && st.GetFrame(i).GetFileLineNumber() > 0; i++)
+                {
+                    if (Debugger.IsAttached == false && st.GetFrame(i).GetMethod().ToString().Substring(0, 2) != "emw") break;
+                    wmsLogMessage(st.GetFrame(i).GetMethod() + " - " + st.GetFrame(i).GetFileLineNumber());
+                }
+
+                if (Debugger.IsAttached && est != null)
+                {
+                    wmsLogMessage("Strack Trace from Exception:");
+                    for (int i = est.FrameCount - 1; i >= 0 && est.GetFrame(i).GetFileLineNumber() > 0; i--)
+                        wmsLogMessage(est.GetFrame(i).GetMethod() + " - " + est.GetFrame(i).GetFileLineNumber());
+                }
+                break;
+
+            case mwsEnumLogLevels.TYPE_WARNING:
+                stringLevel = "Warning";
+                wmsLogMessage(iProductName + " " + logDateTime + " " + stringLevel + ": (" + st.GetFrame(2).GetMethod().Name + ") - " + text);
+                break;
+
+            case mwsEnumLogLevels.TYPE_INFORMATION:
+                stringLevel = "Information";
+                wmsLogMessage(iProductName + " " + logDateTime + " " + stringLevel + ": " + text);
+                break;
+
+            case mwsEnumLogLevels.TYPE_DEBUG:
+                stringLevel = "Debuging";
+
+                String path = st.GetFrame(2).GetMethod().Name + " at " + st.GetFrame(2).GetFileLineNumber();
+                if (path.Equals("pchDebugMessage")) path = st.GetFrame(3).GetMethod().Name;
+
+                wmsLogMessage(iProductName + " " + logDateTime + " " + stringLevel + ": (" + path + ") - " + text);
+                break;
         }
+    }
 
-        public String mwsGetLogLevel()
+
+    private void wmsLogMessage(String text)
+    {
+        mwsSetLogStream();
+        if (iLogStream == null) return;
+        iLogStream.WriteLine(text);
+        iLogStream.Flush();
+    }
+
+    public void mwsSetLogStream()
+    {
+        if (iLogStream == null)
         {
-          return mwsGetLogLevel(this.pLogLevel);
+            iLogStream = new StreamWriter(pLogFile, false);
+            mwsLogFile = pLogFile;
         }
-
-        public void mwsLogMessage(StackTrace est, mwsEnumLogLevels level, String text)
+        if (iLogStream == null)
         {
-            StackTrace st = new StackTrace(true);
-            String stringLevel = String.Empty;
-            String logDateTime = DateTime.Now.ToString();
-
-            // Check current log level
-            if (pLogLevel < level) return;
-
-            switch (level)
-            {
-                case mwsEnumLogLevels.TYPE_FATAL:
-                    stringLevel = "Fatal";
-                    wmsLogMessage(iProductName + " Version " + mwsVersion() + " " + logDateTime + " " + stringLevel + ": (" + st.GetFrame(1).GetMethod().Name + ") - " + text);
-                    break;
-
-                case mwsEnumLogLevels.TYPE_ERROR:
-                    stringLevel = "Error";
-                    wmsLogMessage(iProductName + " Version " + mwsVersion() + " " + logDateTime + " " + stringLevel + ": (" + st.GetFrame(1).GetMethod().Name + ") - " + text);
-                    wmsLogMessage("Strack Trace from error:");
-                    for (int i = 1; i < st.FrameCount && st.GetFrame(i).GetFileLineNumber() > 0; i++)
-                    {
-                        if (Debugger.IsAttached == false && st.GetFrame(i).GetMethod().ToString().Substring(0, 2) != "emw") break;
-                        wmsLogMessage(st.GetFrame(i).GetMethod() + " - " + st.GetFrame(i).GetFileLineNumber());
-                    }
-
-                    if (Debugger.IsAttached && est != null)
-                    {
-                        wmsLogMessage("Strack Trace from Exception:");
-                        for (int i = est.FrameCount - 1; i >= 0 && est.GetFrame(i).GetFileLineNumber() > 0; i--)
-                            wmsLogMessage(est.GetFrame(i).GetMethod() + " - " + est.GetFrame(i).GetFileLineNumber());
-                    }
-                    break;
-
-                case mwsEnumLogLevels.TYPE_WARNING:
-                    stringLevel = "Warning";
-                    wmsLogMessage(iProductName + " " + logDateTime + " " + stringLevel + ": (" + st.GetFrame(2).GetMethod().Name + ") - " + text);
-                    break;
-
-                case mwsEnumLogLevels.TYPE_INFORMATION:
-                    stringLevel = "Information";
-                    wmsLogMessage(iProductName + " " + logDateTime + " " + stringLevel + ": " + text);
-                    break;
-
-                case mwsEnumLogLevels.TYPE_DEBUG:
-                    stringLevel = "Debuging";
-
-                    String path = st.GetFrame(2).GetMethod().Name + " at " + st.GetFrame(2).GetFileLineNumber();
-                    if (path.Equals("pchDebugMessage")) path = st.GetFrame(3).GetMethod().Name;
-
-                    wmsLogMessage(iProductName + " " + logDateTime + " " + stringLevel + ": (" + path + ") - " + text);
-                    break;
-            }
+            mwsLogFile = null;
         }
+    }
 
-
-        private void wmsLogMessage(String text)
-        {
-            mwsSetLogStream();
-            if (iLogStream == null) return;
-            iLogStream.WriteLine(text);
-            iLogStream.Flush();
-        }
-
-        private void mwsSetLogStream()
-        {
-            if (iLogStream == null)
-            {
-                iLogStream = new StreamWriter(pLogFile, false);
-                mwsLogFile = pLogFile;
-            }
-            if (iLogStream == null)
-            {
-                mwsLogFile = null;
-            }
-        }
+    public void mwsStopLogStream()
+    {
+        if (iLogStream != null) iLogStream.Close();
+        File.Delete(mwsLogFile);
+        iLogStream = null;
+    }
+       
     }
 
